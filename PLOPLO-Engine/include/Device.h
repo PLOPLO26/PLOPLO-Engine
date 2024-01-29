fb5615cbedbc0644a692c4695e8dfb8cc07cc90b
PLOPLO26 <124856722+PLOPLO26@users.noreply.github.com>
2024-01-29 10:36:54 -0600

    Device Metdos and Creation Device Contex
    La creacion de las funciones de los metodos que usaremos

--- PLOPLO-Engine/include/Device.h
+++ PLOPLO-Engine/include/Device.h
@@ -22,17 +22,46 @@ public:
     HRESULT 
     CreateRenderTargetView(ID3D11Resource* pResource,
                            const D3D11_RENDER_TARGET_VIEW_DESC* pDesc,
-                            ID3D11RenderTargetView** ppRTView);
+                           ID3D11RenderTargetView** ppRTView);
 
-    HRESULT STDMETHODCALLTYPE CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc,
-                                              const D3D11_SUBRESOURCE_DATA* pInitialData,
-                                              ID3D11Texture2D** ppTexture2D);
+    HRESULT 
+    CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc,
+                    const D3D11_SUBRESOURCE_DATA* pInitialData,
+                    ID3D11Texture2D** ppTexture2D);
 
-    HRESULT STDMETHODCALLTYPE CreateDepthStencilView(ID3D11Resource* pResource,
-                                                     const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
-                                                     ID3D11DepthStencilView** ppDepthStencilView);
+    HRESULT  
+    CreateDepthStencilView(ID3D11Resource* pResource,
+                           const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
+                           ID3D11DepthStencilView** ppDepthStencilView);
 
+    HRESULT 
+    CreateVertexShader(const void* pShaderBytecode,
+                       unsigned int BytecodeLength,
+                       ID3D11ClassLinkage* pClassLinkage,
+                       ID3D11VertexShader** ppVertexShader);
+    HRESULT 
+    CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs,
+                      unsigned int NumElements,
+                      const void* pShaderBytecodeWithInputSignature,
+                      unsigned int BytecodeLength,
+                      ID3D11InputLayout** ppInputLayout);
 
+    HRESULT 
+    CreatePixelShader(const void* pShaderBytecode,
+                      unsigned int BytecodeLength,
+                      ID3D11ClassLinkage* pClassLinkage,
+                      ID3D11PixelShader** ppPixelShader);
+
+    HRESULT 
+    CreateBuffer(const D3D11_BUFFER_DESC* pDesc,
+                 const D3D11_SUBRESOURCE_DATA* pInitialData,
+                 ID3D11Buffer** ppBuffer);
+
+    HRESULT 
+    CreateSamplerState(const D3D11_SAMPLER_DESC* pSamplerDesc,
+                       ID3D11SamplerState** ppSamplerState);
+
+   
 
 public:
     ID3D11Device* m_device = nullptr;
--- PLOPLO-Engine/src/Device.cpp
+++ PLOPLO-Engine/src/Device.cpp
@@ -78,3 +78,144 @@ HRESULT Device::CreateDepthStencilView(ID3D11Resource* pResource,
 	}
 	return hr;
 }
+
+HRESULT Device::CreateVertexShader(const void* pShaderBytecode, 
+	                               unsigned int BytecodeLength, 
+	                               ID3D11ClassLinkage* pClassLinkage, 
+	                               ID3D11VertexShader** ppVertexShader)
+{
+	HRESULT hr = S_OK;
+
+	if (pShaderBytecode == nullptr)
+	{
+		WARNING("ERROR: Device::CreateVertexShader : Error in data from params [CHECK FOR const void* pShaderBytecode] \n");
+		exit(1);
+	}
+	else if (ppVertexShader == nullptr)
+	{
+		WARNING("ERROR: Device::CreateVertexShader : Error in data from params [CHECK FOR ID3D11VertexShader** ppVertexShader] \n");
+		exit(1);
+	}
+	else
+	{
+		hr = m_device->CreateVertexShader(pShaderBytecode,
+										  BytecodeLength,
+			                              pClassLinkage,
+			                              ppVertexShader);
+		WARNING("Device::CreateVertexShader : [CREATION OF RESOURCE : OK] \n");
+	}
+
+	return hr;
+}
+
+HRESULT Device::CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs, 
+	                              unsigned int NumElements, 
+	                              const void* pShaderBytecodeWithInputSignature, 
+	                              unsigned int BytecodeLength, 
+	                              ID3D11InputLayout** ppInputLayout)
+{
+	HRESULT hr = S_OK;
+	if (pInputElementDescs == nullptr)
+	{
+		WARNING("ERROR: Device::CreateInputLayout : Error in data from params [CHECK FOR CONST D3D11_INPUT_ELEMENT_DESC* pInputDescs]\n");
+		exit(1);
+	}
+	else if(pShaderBytecodeWithInputSignature == nullptr)
+	{
+		WARNING("ERROR: Device::CreateInputLayout : Error in data from params [CHECK FOR const void* pShaderBytecodeWithInputSignature]\n");
+		exit(1);
+	}
+	else if (ppInputLayout == nullptr)
+	{
+		WARNING("ERROR: Device::CreateInputLayout : Error in data from params [CHECK FOR ID3D11InputLayout** ppInputLayout]\n");
+		exit(1);
+	}
+	else
+	{
+		hr = m_device->CreateInputLayout(pInputElementDescs,
+			                             NumElements,
+			                             pShaderBytecodeWithInputSignature,
+			                             BytecodeLength,
+			                             ppInputLayout);
+		WARNING("Device::CreateInputLayout : [CREATION OF RESOURCE : OK ] \n");
+	}
+	return hr;
+}
+
+HRESULT Device::CreatePixelShader(const void* pShaderBytecode,
+	                              unsigned int BytecodeLength,
+	                              ID3D11ClassLinkage* pClassLinkage,
+	                              ID3D11PixelShader** ppPixelShader)
+{
+	HRESULT hr = S_OK;
+
+	if (pShaderBytecode == nullptr)
+	{
+		WARNING("ERROR: Device::CreatePixelShader : Error in data from params [CHECK FOR const void* pShaderBytecode] \n");
+		exit(1);
+	}
+	else if (ppPixelShader == nullptr)
+	{
+		WARNING("ERROR: Device::CreatePixelShader : Error in data from params [CHECK FOR ID3D11PixelShader** ppPixelShader] \n");
+		exit(1);
+	}
+	else
+	{
+		hr = m_device->CreatePixelShader(pShaderBytecode,
+			                             BytecodeLength,
+			                             pClassLinkage,
+			                             ppPixelShader);
+		WARNING("Device::CreatePixelShader : [CREATION OF RESOURCE : OK] \n");
+	}
+	return hr;
+}
+
+HRESULT Device::CreateBuffer(const D3D11_BUFFER_DESC* pDesc, 
+	                         const D3D11_SUBRESOURCE_DATA* 
+	                         pInitialData, ID3D11Buffer** ppBuffer)
+{
+	HRESULT hr = S_OK;
+
+	if (pDesc == nullptr)
+	{
+		WARNING("ERROR: Device::CreateBuffer : Error in data from params [CHECK FOR const D3D11_BUFFER_DESC* pDesc] \n");
+		exit(1);
+	}
+	else if (ppBuffer == nullptr)
+	{
+		WARNING("ERROR: Device::CreateBuffer : Error in data from params [CHECK FOR ID3D11Buffer** ppBuffer ] \n");
+		exit(1);
+	}
+	else
+	{
+		hr = m_device->CreateBuffer(pDesc, pInitialData, ppBuffer);
+		WARNING("Device::CreateBuffer : [CREATION OF RESOURCE : OK] \n");
+	}
+
+	return hr;
+}
+
+HRESULT Device::CreateSamplerState(const D3D11_SAMPLER_DESC* pSamplerDesc, 
+	                               ID3D11SamplerState** ppSamplerState)
+{
+	HRESULT hr = S_OK;
+
+	if (pSamplerDesc == nullptr)
+	{
+		WARNING("ERROR: Device::CreateSamplerState : Error in data from params [CHECK FOR const D3D11_SAMPLE_DESC* pSamplerDesc] \n");
+		exit(1);
+	}
+	else if (ppSamplerState == nullptr)
+	{
+		WARNING("ERROR: Device::CreateSamplerState : Error in data from params [CHECK FOR const ID3D11SamplerState** ppSamplerState] \n");
+		exit(1);
+	}
+	else
+	{
+		hr = m_device->CreateSamplerState(pSamplerDesc, ppSamplerState);
+		WARNING("Device::CreateSamplerState : [CREATION OF RESOURCE : OK] \n");
+	}
+
+	return hr;
+}
+ 
